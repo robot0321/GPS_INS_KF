1e44ac6b494edf124da1cb298f5c7a733d059bc7
robot0321 <jychung0321@gmail.com>
2016-07-03 23:21:17 +0900

    time acc gyro data sampling

--- imuKF.cpp
+++ imuKF.cpp
@@ -1,31 +1,62 @@
+#include <fstream>
 #include <iostream>
 #include <mpu9250.h>
+#include <ctime>
+//#include <signal.h>
+//#include <cstdlib>
+//#include <stdio.h>
+//#include <unistd.h>
 
 using namespace std;
+
+//void signalHandler(sig_t s);
+ofstream outFile("output.txt");
 
 int main(int arg, char **argv){
 	float accData[3];
 	float gyroData[3];
-	
+
+	//signal(SIGINT, signalHandler); //Catch Keyboard Interrupt
+
 	cout<<"Kalman Filter"<<endl;
 	
 	MPU9250 mpu = MPU9250();
 	mpu.mpu9250Initialize();
+
 	
 	while(1){
+		clock_t time = clock();
+		cout<<time<<" ";
+		outFile<<time<<"\t";
+		
 		mpu.mpu9250read_acc(accData);
 		for(int i=0;i<3;i++){
 			cout<<accData[i]<<" ";
+			outFile<<accData[i]<<"\t";
 		}
 		mpu.mpu9250read_gyro(gyroData);
 		for(int i=0;i<3;i++){
 			cout<<gyroData[i]<<" ";
+			outFile<<gyroData[i]<<"\t";
 		}
 		cout<<endl;
+		outFile<<endl;
 		
 
 
 	}
 }
 
+
+
+/*
+void signalHandler(int signum){
+	cout<<"Interrupt signal "<<signum<<endl;
+	try{
+		outFile.close();
+	} catch(int exception){
+	}
+	exit(signum);
+}
+*/
 
